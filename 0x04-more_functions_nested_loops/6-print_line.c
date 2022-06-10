691567439c8eca7b210cdb3c44bc1ef8b3f7bfbd
Agatha <agkaranja5@gmail.com>
2022-06-09 18:10:10 -0700

    Print a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_line - Print a straight line on the command
+ *@n: number of times to print
+ */
+void print_line(int n)
+{
+	while (n < 10)
+	{
+		_putchar('_');
+		n++;
+	}
+}
