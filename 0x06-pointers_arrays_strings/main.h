4d10991d358bfae4df3584986c7af97bb22aa4b8
Agatha <agkaranja5@gmail.com>
2022-06-16 19:11:19 -0700

    Print an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,3 +4,5 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *leet(char *);
+int _putchar(char c);
+void print_number(int n);
