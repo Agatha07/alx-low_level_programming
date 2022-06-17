e8fac89f0f84bc96952b545277397b2e02aad66d
Agatha <agkaranja5@gmail.com>
2022-06-16 19:57:46 -0700

    Dereference

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +8,4 @@ char *string_toupper(char *);
 char *leet(char *);
 int _putchar(char c);
 void print_number(int n);
+
