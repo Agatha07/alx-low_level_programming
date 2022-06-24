7a5ec4900cb47a302b77993e0013cd2220d8e952
Agatha <agkaranja5@gmail.com>
2022-06-24 16:58:09 -0700

    Print all arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * @argc: Counts arguments
+ * @argv: Arguments received
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		ptintf("%s\n", argv[c]);
+	return (0);
+}
