6e44f7eabb1120566af4f2e2ead49ebe5342ed96
Agatha <agkaranja5@gmail.com>
2022-07-21 15:58:54 -0700

    Update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 int get_endianness(void);
 int get_endianness(void);
 int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
 
 #endif
