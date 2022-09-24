7b9e0a37befe9bbddd911b6a87a8ac885f31e23e
Agatha07 <agkaranja5@gmail.com>
2022-09-23 18:49:25 -0700

    Find the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,8 @@
  * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
  *
- * Return: the index at which the key/value pair should be stored in the array of the hash table
+ * Return: the index at which the key/value pair should be 
+ * stored in the array of the hash table
  *
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
